d7524d5421c5908d993fa9bf98b4a544c0030fe4
PratibhaShalini <35136370+PratibhaShalini@users.noreply.github.com>
2018-01-06 17:45:49 +0530

    Add files via upload

---
+++ notes.js
@@ -0,0 +1,19 @@
+/*console.log ('start the App');
+console.log (module);
+module.exports.age = 25;*/
+/*module.exports.addNote = ()=>
+{
+    console.log('Adding Note');
+return 'New Note'; // Returning a string
+}*/
+/*module.exports.add = (a,b)=> {
+   console.log('Adding Note');
+return a+b; // Returning a string
+}*/
+console.log(module);
+const id= module.id;
+// console.log(id);
+os = require('os');
+//var os1 = os.userInfo();
+// module.id =os1;
+console.log(id);
